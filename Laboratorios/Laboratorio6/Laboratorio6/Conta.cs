c473b66dc4cdeac504e253e9099ccbbb14f79dd4
rafayoko <rafaelatakitani@outlook.com>
2021-11-24 11:47:48 -0300

    lab6

---
+++ Laboratorios/Laboratorio6/Laboratorio6/Conta.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio6
+{
+    public abstract class Conta
+    {
+        private decimal saldo;
+        private string titular;
+       
+
+        public Conta(string t, decimal s)
+        {
+            titular = t;
+            saldo = s;
+           
+        }
+     
+        public decimal Saldo
+        {
+            get { return saldo; }
+        }
+        public string Titular
+        {
+            get { return titular; }
+        }
+
+        public abstract string Id
+        {
+            get;
+        }
+        public virtual void Depositar(decimal valor)
+        {
+            saldo += valor;
+        }
+        public virtual void Sacar(decimal valor)
+        {
+            saldo -= valor;
+        }
+    }
+}
---
+++ Laboratorios/Laboratorio6/Laboratorio6/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laboratorio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  
+            1.Escreva um programa que crie vários tipos de conta e teste cada um dos métodos e propriedades
+            desenvolvidos.
+
+           */
+
+            Console.WriteLine("Deseja cadastrar uma conta? (s/n)");
+            char cadastro = char.Parse(Console.ReadLine());
+
+            while (cadastro == 's')
+            {
+               
+                Console.WriteLine("Qual o nome do Titular? ");
+                string nometitular = Console.ReadLine();
+
+                Console.WriteLine(" Qual o  depósito inicial? (s/n)");
+                decimal depoinicial = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("Qual a taxa de juros?");
+                decimal taxadejurosconta = decimal.Parse(Console.ReadLine());
+                ContaPoupanca cp = new ContaPoupanca(nometitular, taxadejurosconta);
+
+                DateTime datadeniver = DateTime.Now;
+                
+                List<Conta> list = new List<Conta>();
+                list.Add(new ContaPoupanca(taxadejurosconta, datadeniver, nometitular, depoinicial));
+
+                foreach (Conta ac in list)
+                {
+                    Console.WriteLine("Dados:" + ac.Titular) ;
+                }
+
+
+
+                Console.WriteLine("Deseja cadastrar outra conta? (s/n)");
+                cadastro = char.Parse(Console.ReadLine());
+            }
+
+
+
+
+            /*
+            2.Crie uma coleção de objetos do tipo Conta e acrescente diversos objetos dos tipos das classes
+            derivadas.Quais métodos podem ser chamados sobre os elementos da coleção?*/
+
+
+                }
+    }
+}
